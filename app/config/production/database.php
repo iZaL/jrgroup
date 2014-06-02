b304b61ec1513b13ded0c3ccd9d888fe142ed8e6
ZaL <z4ls@live.com>
2014-06-02 18:40:26 +0300

    added db conf

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -4,14 +4,14 @@ return array(
 
 	'connections' => array(
 		'mysql' => array(
-			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => '',
-			'username'  => '',
-			'password'  => '',
-			'charset'   => 'utf8',
-			'collation' => 'utf8_unicode_ci',
-			'prefix'    => '',
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'jrgroup',
+            'username'  => 'jrgroup-db',
+            'password'  => 'jrgroupkw2014',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
 		),
 
 	)
