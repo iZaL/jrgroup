ef90cadf5a8accaff55cff2212c583127738fa55
ZaL <z4ls@live.com>
2014-06-02 19:13:27 +0300

    fix admin datatable

--- app/views/admin/comments/index.blade.php
+++ app/views/admin/comments/index.blade.php
@@ -40,22 +40,4 @@
         </table>
     </div>
 
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('.datatable').dataTable({
-                "sPaginationType": "bs_four_button"
-            });
-            $('.datatable').each(function(){
-                var datatable = $(this);
-                // SEARCH - Add the placeholder for Search and Turn this into in-line form control
-                var search_input = datatable.closest('.dataTables_wrapper').find('div[id$=_filter] input');
-                search_input.attr('placeholder', 'Search');
-                search_input.addClass('form-control input-md');
-                // LENGTH - Inline-Form control
-                var length_sel = datatable.closest('.dataTables_wrapper').find('div[id$=_length] select');
-                length_sel.addClass('form-control input-sm');
-            });
-        });
-    </script>
 @stop
