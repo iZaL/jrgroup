bd7ad282746d057c025bf416c0ccea02599d316a
ZaL <z4ls@live.com>
2014-07-03 20:48:17 +0300

    added locale

--- app/views/site/partials/breadcrumb.blade.php
+++ app/views/site/partials/breadcrumb.blade.php
@@ -2,5 +2,6 @@
     <ol class="breadcrumb">
         <li><i class="fa fa-home"></i>&nbsp;<a href="{{ action('HomeController@index') }}">{{ Lang::get('site.nav.home') }}</a></li>
         @yield('breadcrumb')
+        @include('site.partials.locale')
     </ol>
 </div>
