c9e7211f92c627f19b8b4ad61b05c7e6d41b16cc
ZaL <z4ls@live.com>
2014-11-17 12:47:38 +0300

    clean database seeder

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,39 +2,39 @@
 
 class DatabaseSeeder extends Seeder {
 
-//    private $tables = [
-//        'assigned_roles',
-//        'authors',
-//        'categories',
-//        'comments',
-//        'contacts',
-//        'countries',
-//        'events',
-//        'failed_jobs',
-//        'favorites',
-//        'followers',
-//        'locations',
-//        'migrations',
-//        'password_reminders',
-//        'roles',
-//        'permission_role',
-//        'permissions',
-//        'photos',
-//        'posts',
-//        'subscriptions',
-//        'users',
-//        'types',
-//        'statuses',
-//        'certificate_types',
-//        'certificate_metas',
-//        'certificate_options',
-//        'certificate_requests',
-//        'certificate_statuses',
-//        'certificate_option_type',
-//        'certificate_request_options',
-//        'galleries'
-//
-//    ];
+    private $tables = [
+        'assigned_roles',
+        'authors',
+        'categories',
+        'comments',
+        'contacts',
+        'countries',
+        'events',
+        'failed_jobs',
+        'favorites',
+        'followers',
+        'locations',
+        'migrations',
+        'password_reminders',
+        'roles',
+        'permission_role',
+        'permissions',
+        'photos',
+        'posts',
+        'subscriptions',
+        'users',
+        'types',
+        'statuses',
+        'certificate_types',
+        'certificate_metas',
+        'certificate_options',
+        'certificate_requests',
+        'certificate_statuses',
+        'certificate_option_type',
+        'certificate_request_options',
+        'galleries'
+
+    ];
     public function run()
     {
         Eloquent::unguard();
@@ -43,38 +43,38 @@ class DatabaseSeeder extends Seeder {
         $this->call('UsersTableSeeder');
         $this->call('RolesTableSeeder');
         $this->call('PermissionsTableSeeder');
-        $this->call('CountriesTableSeeder');
-        $this->call('LocationsTableSeeder');
-        $this->call('CategoriesTableSeeder');
-        $this->call('PostsTableSeeder');
-		$this->call('EventsTableSeeder');
-        $this->call('CommentsTableSeeder');
+//        $this->call('CountriesTableSeeder');
+//        $this->call('LocationsTableSeeder');
+//        $this->call('CategoriesTableSeeder');
+//        $this->call('PostsTableSeeder');
+//		$this->call('EventsTableSeeder');
+//        $this->call('CommentsTableSeeder');
 //        $this->call('FollowersTableSeeder');
 //		$this->call('FavoritesTableSeeder');
 //		$this->call('SubscriptionsTableSeeder');
 //		$this->call('PhotosTableSeeder');
 //		$this->call('AuthorsTableSeeder');
-        $this->call('ContactsTableSeeder');
-        $this->call('StatusesTableSeeder');
-        $this->call('TypesTableSeeder');
-        $this->call('CertificateTypesTableSeeder');
-        $this->call('CertificateMetasTableSeeder');
-        $this->call('CertificateOptionsTableSeeder');
-        $this->call('CertificateOptionTypeTableSeeder');
-        $this->call('CertificateRequestsTableSeeder');
-        $this->call('CertificateStatusesTableSeeder');
-        $this->call('CertificateRequestOptionTableSeeder');
+//        $this->call('ContactsTableSeeder');
+//        $this->call('StatusesTableSeeder');
+//        $this->call('TypesTableSeeder');
+//        $this->call('CertificateTypesTableSeeder');
+//        $this->call('CertificateMetasTableSeeder');
+//        $this->call('CertificateOptionsTableSeeder');
+//        $this->call('CertificateOptionTypeTableSeeder');
+//        $this->call('CertificateRequestsTableSeeder');
+//        $this->call('CertificateStatusesTableSeeder');
+//        $this->call('CertificateRequestOptionTableSeeder');
 //        $this->call('GalleriesTableSeeder');
 	}
 
-//    private function cleanDatabase()
-//    {
-//        DB::statement('SET FOREIGN_KEY_CHECKS=0');
-//        foreach ($this->tables as $table) {
-//            DB::table($table)->truncate();
-//        }
-//        DB::statement('SET FOREIGN_KEY_CHECKS=1');
-//
-//    }
+    private function cleanDatabase()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        foreach ($this->tables as $table) {
+            DB::table($table)->truncate();
+        }
+        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+
+    }
 
 }
