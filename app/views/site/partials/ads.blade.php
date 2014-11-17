1183134026178765f5b767f7679d680631cc5daf
ZaL <z4ls@live.com>
2014-11-17 18:37:46 +0300

    before major refactor

--- app/views/site/partials/ads.blade.php
+++ app/views/site/partials/ads.blade.php
@@ -1,25 +1,23 @@
 <!-- Advertisment section-->
-
-    <div id="ads-section" class="row hidden-sm hidden-xs top10">
-        @if($ad1)
-        <div id="ads-1" class="col-md-6 ">
+<div id="ads-section" class="row hidden-sm hidden-xs top10">
+    @if($ad1)
+        <div id="ads-1" class="col-md-6">
             <div class="ads" >
-                {{ HTML::image('uploads/'.$ad1.'','image1',array('class'=>'img-responsive img-thumbnail')) }}
+                {{ HTML::image('uploads/'.$ad1,'image1',array('class'=>'img-responsive img-thumbnail')) }}
             </div>
         </div>
-        @else
+    @else
         <div id="ads-1" class="col-md-6"><img class="img-responsive" src=" http://placehold.it/360x150"/></div>
-        @endif
+    @endif
 
-        @if($ad2)
+    @if($ad2)
         <div id="ads-2" class="col-md-6">
-
             <div class="ads" >
-                {{ HTML::image('uploads/'.$ad2.'','image2',array('class'=>'img-responsive img-thumbnail')) }}
+                {{ HTML::image('uploads/'.$ad2,'image2',array('class'=>'img-responsive img-thumbnail')) }}
             </div>
         </div>
-        @else
+    @else
         <div id="ads-2" class="col-md-6"><img class="img-responsive" src=" http://placehold.it/360x150"/></div>
-        @endif
-    </div>
+    @endif
+</div>
 
